8a8cd69be0e9393075bdb82954ca5032a3db71c3
michellemwangi01 <michellemwangim@gmail.com>
2023-06-05 14:08:27 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(){
+    
+}
+function subtract(){ 
+    
+}
+function multiply(){
+    
+}
+function divide(){
+    
+}
+
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){ 
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+
+}
+function divide(a,b){
+    return a / b;
+}
+function increment(n){
+    return ++n;
+}
+function decrement(n){
+    return --n;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+
+}console.log(makeInt(9.8778));
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+console.log(preserveDecimal("10"));
+
+
